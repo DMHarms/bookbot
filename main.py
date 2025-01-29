5f4f3a330e25e5b22f9591d6bf6c7e46fdcae78b
DMHarms <dmharms@gmail.com>
2025-01-29 15:05:55 -0800

    Added funtions to get the file and count words of that file

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    with open("books/frankenstein.txt") as f:
+file_contents = ""
+source = "books/frankenstein.txt"
+
+def get_file():
+    with open(source) as f:
         file_contents = f.read()
-        return print(file_contents)
-    
+        return file_contents
+        
+def count_words():
+    content = get_file()
+    words = content.split()
+    word_count = len(words)
+    print(f"word count: {word_count}")
+
+def main():
+    count_words()
+
 main()
