ca5a6b2d259cb6cc0e81a20fe7c107aa97f70ee1
DMHarms <dmharms@gmail.com>
2025-01-29 16:50:03 -0800

    Added function to count characters in the given file

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-file_contents = ""
 source = "books/frankenstein.txt"
 
-def get_file():
-    with open(source) as f:
-        file_contents = f.read()
-        return file_contents
-        
 def count_words():
     content = get_file()
     words = content.split()
     word_count = len(words)
     print(f"word count: {word_count}")
 
+def count_characters():
+    content = get_file()
+    lowercase_content = content.lower()
+    d_characters = {}
+    for i in lowercase_content:
+        d_characters[i] = 0
+    for i in lowercase_content:
+        d_characters[i] += 1
+    print(d_characters)
+
+def get_file():
+    with open(source) as f:
+        file_contents = f.read()
+        return file_contents
+
 def main():
-    count_words()
+    # count_words()
+    count_characters()
 
 main()
