0e2ca089b4c856b8a9f7925d24dd3b088b278533
DMHarms <dmharms@gmail.com>
2025-01-30 23:32:13 -0800

    Added functions to report only alphabetical character counts and formatted report

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 source = "books/frankenstein.txt"
 
+print(f"=== Report of {source} ===")
+
+def get_file():
+    with open(source) as f:
+        file_contents = f.read()
+        return file_contents
+
 def count_words():
     content = get_file()
     words = content.split()
     word_count = len(words)
-    print(f"word count: {word_count}")
+    print(f"Word count: {word_count}")
 
 def count_characters():
     content = get_file()
@@ -14,15 +21,24 @@ def count_characters():
         d_characters[i] = 0
     for i in lowercase_content:
         d_characters[i] += 1
-    print(d_characters)
+    return d_characters
 
-def get_file():
-    with open(source) as f:
-        file_contents = f.read()
-        return file_contents
+def sort_dict(unsorted_dict):
+    unsorted_list = list(unsorted_dict.items())
+    sorted_list = sorted(unsorted_list, key=lambda x: x[1])
+    return sorted_list
+
+def character_report():
+    character_list = sort_dict(count_characters())
+    sorted_dict = dict(character_list)
+    for i in sorted_dict:
+        if i.isalpha():
+            print(f"The letter '{i}' was found {sorted_dict[i]} times")
 
 def main():
-    # count_words()
-    count_characters()
+    count_words()
+    character_report()
 
 main()
+
+print("=== END ===")
